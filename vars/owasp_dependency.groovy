4e070876b871284245d385293dd48844d7c4a31a
AbdulManan Ali <132811477+abdulmanan-ali@users.noreply.github.com>
2026-01-09 23:42:42 +0500

    Modify OWASP Dependency Check output directory
    Updated OWASP Dependency Check script to output to current directory.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,11 +1,14 @@
 def call() {
     echo "Running OWASP Dependency Check..."
 
-    sh '''
-        dependency-check.sh \
+    // Tool name MUST match Jenkins → Manage Jenkins → Tools
+    def dcHome = tool 'OWASP'
+
+    sh """
+        ${dcHome}/bin/dependency-check.sh \
         --project "Wanderlust" \
         --scan . \
         --format XML \
-        --out dependency-check-report
-    '''
+        --out .
+    """
 }
