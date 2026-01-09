e3903d00d169fc32f28aa24fb2609bf52bf74945
AbdulManan Ali <132811477+abdulmanan-ali@users.noreply.github.com>
2026-01-09 23:25:01 +0500

    Add SonarQube analysis function in Groovy

---
+++ vars/sonarqube_analysis.groovy
@@ -0,0 +1,14 @@
+def call(String sonarTool, String projectKey, String projectName) {
+    echo "Running SonarQube Code Analysis..."
+
+    def scannerHome = tool sonarTool
+
+    withSonarQubeEnv(sonarTool) {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.sources=.
+        """
+    }
+}
