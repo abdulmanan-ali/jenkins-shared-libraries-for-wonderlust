a51e5dbcdfb8866caea8b5d076a5e0dd31b09148
AbdulManan Ali <132811477+abdulmanan-ali@users.noreply.github.com>
2026-01-12 00:48:37 +0500

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]) {
+    echo ">>> USING MAP-BASED docker_build FROM SHARED LIBRARY <<<"
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
     def dockerfile = config.dockerfile ?: 'Dockerfile'
